a63f94a135e1b344df7a246282b7da6d62620ac9
prakhar33 <38583162+prakhar33@users.noreply.github.com>
2019-01-26 19:56:49 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,6 +20,10 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+from sklearn import svm
+clf = svm.SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
 
 
 #########################################################
